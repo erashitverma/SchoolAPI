c48956b12a8aa8306cf765672b0ca9d929e4a8fb
Ashit K Verma <contact.ashitverma@gmail.com>
2021-01-06 13:21:54 +0000

    Fixed errors due to file deletion

--- src/main/java/com/asverma/core/services/impl/AbstractService.java
+++ src/main/java/com/asverma/core/services/impl/AbstractService.java
@@ -1,17 +1,11 @@
 package com.asverma.core.services.impl;
 
 import com.asverma.core.services.CRUDService;
-import com.asverma.db.WrapperInterface;
 
 import java.util.List;
 
+// Unused at the moment due to time restriction //
 public class AbstractService<T> implements CRUDService<T> {
-
-    private final WrapperInterface wrapperInterface;
-
-    public AbstractService(final WrapperInterface wrapperInterface) {
-        this.wrapperInterface = wrapperInterface;
-    }
 
     @Override public T retrieveEntity(final int id) {
         return null;
@@ -25,9 +19,7 @@ public class AbstractService<T> implements CRUDService<T> {
         return null;
     }
 
-    @Override public void deleteEntity(final int id) {
-
-    }
+    @Override public void deleteEntity(final int id) { }
 
     @Override public List<T> retrieveAll() {
         return null;
